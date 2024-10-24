af2d465f41ac6d9abd74366ed13ba668c40fd5ea
dedeamor12 <andreleandromatos@gmail.com>
2024-10-24 09:07:33 -0300

    21/10 ok

--- app.py
+++ app.py
@@ -9,53 +9,61 @@ app = Flask(__name__)
 def hide_message(message, image):
     encoded = image.copy()
     width, height = image.size
-    pixels =encoded.load() 
+    pixels = encoded.load()
 
-    # Converter a mensagem para binario
-    binary_message = ''.join(format(ord(i), '08b')for i in message)
+    # Converter a mensagem para binário
+    binary_message = ''.join(format(ord(i), '08b') for i in message)
     binary_message += '1111111111111110' #terminador
 
     data_index = 0
     for y in range(height):
         for x in range(width):
             pixel = list(pixels[x, y])
-            for i in range(3): #ALterar os primeiros 3 valores do pixel
-                if data_index <len(binary_message):
+            for i in range(3): #Alterar os 3 primeiros valores RGB do pixel
+                if data_index < len(binary_message):
                     pixel[i] = pixel[i] & -1 | int(binary_message[data_index])
-                    data_index += 1 
+                    data_index += 1
+                
             pixels[x, y] = tuple(pixel)
             if data_index >= len(binary_message):
                 break
+        if data_index >=len(binary_message):
+            break
     return encoded
 
 @app.template_filter('b64encode')
 def b64encode_filter(data):
-    return  base64.b64encode(data).decode('utf-8')
+    return base64.b64encode(data).decode('utf-8')
 
-#Rota principal
+# Rota principal
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#Rota codificar
-@app.route('/codificar', methods=['GET', 'POST'])
-def codificar():
+# Rota codificar
+@app.route('/encode', methods=['GET', 'POST'])
+def encode():
     if request.method == 'POST':
-        file = request.files['iamge']
-        message =request.form['message']
+        file = request.files['image']
+        message = request.form['message']
 
         #Abrir a imagem e ocultar a mensagem
         img = Image.open(file)
         encoded_img = hide_message(message, img)
 
-        #Nome da imagem com timestamp
-        timestamp = str(int(time.time))
+        # Nome da imagem com timestamp
+        timestamp = str(int(time.time()))
         img_filename = f'encoded_{timestamp}.png'
 
-# Salvar a imagem codificada em um objeto de 
-    img_io = io.BytesIO()
-    encoded_img.save(img_io, 'PNGi')
-    img_io.seek(0)
+        # Salvar a imagem codificada em um objeto de bytes
+        img_io = io.BytesIO()
+        encoded_img.save(img_io, 'PNG')
+        img_io.seek(0)
 
-    return render_template('decodificar.html', image_data=img_io.getvalue(), img_filename=img_filename)
-    return render_template('codificar.html')
+        return render_template(
+            'encode.html',
+            image_data=img_io.getvalue(),
+            img_filename=img_filename
+        )
+
+    return render_template('encode.html')
